dfcb1ddb5fdb00078579911a71bd2df3425d87c2
Jonathan Matsson <jmatss@kth.se>
2019-08-26 16:53:58 +0200

    Change packaging structure

--- cmd/createsortedwordlist/main.go
+++ cmd/createsortedwordlist/main.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	MaxAmountOfConcurrentThreads = 8
-	AmountOfBlocks               = 4
+	AmountOfBlocks               = 1
 	Filename                     = "d:\\listdir\\list"
 	BufferSize                   = 1000000
 )
@@ -18,12 +18,5 @@ func main() {
 	end := 0xffffff
 	startTime := time.Now()
 	blocks := lib.CreateBlocks(start, end, AmountOfBlocks, MaxAmountOfConcurrentThreads, BufferSize, Filename)
-	/*
-		for _, block := range blocks {
-			for _, hashDigest := range (*block).Hashes {
-				fmt.Printf("%s %s\n", hex.EncodeToString([]byte(hashDigest[:]))[:20], hex.EncodeToString([]byte(hashDigest[:]))[20:])
-			}
-		}
-	*/
 	fmt.Printf("%v (happy compiler:%p)\n", time.Since(startTime), blocks)
 }
