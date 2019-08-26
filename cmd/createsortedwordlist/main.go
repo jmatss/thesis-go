47115cd1c51643a5c6854487cab31798174e1266
Jonathan Matsson <jmatss@kth.se>
2019-08-26 20:16:29 +0200

    Changed main to receive errors

--- cmd/createsortedwordlist/main.go
+++ cmd/createsortedwordlist/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/jmatss/thesis-go/pkg/createblocks"
+	"log"
 	"time"
+
+	"github.com/jmatss/thesis-go/pkg/createblocks"
 )
 
 const (
@@ -16,7 +17,28 @@ const (
 func main() {
 	start := 0
 	end := 0xffffff
+
+	/*
+		STAGE 1 - Create blocks that has a size that can fit in the memory
+		Generate, sort and write hashes of the block to separate files
+	*/
 	startTime := time.Now()
-	blocks := createblocks.Create(start, end, AmountOfBlocks, MaxAmountOfConcurrentThreads, BufferSize, Filename)
-	fmt.Printf("%v (happy compiler:%p)\n", time.Since(startTime), blocks)
+	blocks, err := createblocks.Create(start, end, AmountOfBlocks, MaxAmountOfConcurrentThreads, BufferSize, Filename)
+	if err != nil {
+		log.Fatalf("could not create blocks: %v", err)
+	}
+	if blocks != AmountOfBlocks {
+		log.Fatalf("created incorrect amount of blocks, want: %d, got: %d", AmountOfBlocks, blocks)
+	}
+	log.Printf("all blocks created, elapsed time: %v\n", time.Since(startTime))
+
+	/*
+		STAGE 2 - Merge the blocks in to one single sorted file
+	*/
+	startTime = time.Now()
+	if err := createblocks.Merge(blocks, MaxAmountOfConcurrentThreads, Filename); err != nil {
+		log.Fatalf("could not merge blocks: %v", err)
+	}
+	log.Printf("all blocks merged, elapsed time: %v\n", time.Since(startTime))
+
 }
