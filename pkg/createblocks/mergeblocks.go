1cb449cf5f47dc9e9ccebccf96eed80903488466
Jonathan Matsson <jmatss@kth.se>
2019-09-01 17:42:51 +0200

    Merge now returns size of file

--- pkg/createblocks/mergeblocks.go
+++ pkg/createblocks/mergeblocks.go
@@ -4,46 +4,59 @@ import (
 	"bufio"
 	"crypto/md5"
 	"fmt"
+	"log"
 	"os"
 )
 
 const WriterBufferSize = 1000
 
-func Merge(amountOfBlocks, concurrentThreads, blockBufferSize int, filename string) error {
+func Merge(amountOfBlocks, concurrentThreads, blockBufferSize int, filename string, printAmount int) (int, error) {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
-		return fmt.Errorf("not able to open file %s: %v", filename, err)
+		return 0, fmt.Errorf("not able to open file %s: %v", filename, err)
 	}
 
 	writeBuffer := bufio.NewWriterSize(file, WriterBufferSize)
 	defer func() {
-		writeBuffer.Flush()
 		file.Close()
 	}()
 
-	// start handler that will do all comparisong/merging
+	// start handler that will do all comparison/merging
 	// this main thread will fetch the min from the minResult channel and do a buffered file write
 	minResult := make(chan hashDigest)
 	go MergeHandler(amountOfBlocks, concurrentThreads, blockBufferSize, filename, minResult)
 
+	count := 0
 	for {
-		min := <-minResult
 		// if min empty: time to exit, everything done
+		// TODO: can not be sure that all hashes has been merged, implement better error detection
+		min := <-minResult
 		if min == (hashDigest{}) {
 			break
 		}
 
 		n, err := writeBuffer.Write(min[:])
 		if err != nil {
-			return fmt.Errorf("not able to write digest %016x to file %s: %v",
+			return 0, fmt.Errorf("not able to write digest %016x to file %s: %v",
 				min, filename, err)
 		}
 		if n != md5.Size {
-			return fmt.Errorf("incorrect amount of bytes written to file when writing "+
+			return 0, fmt.Errorf("incorrect amount of bytes written to file when writing "+
 				"digest %016x to file %s, expected: %d, wrote: %d: %v",
 				min, filename, md5.Size, n, err)
 		}
+
+		count++
+		if count%printAmount == 0 {
+			log.Printf("%d hashes merged", count)
+		}
 	}
 
-	return nil
+	// flush to file and return size of file on disk so that the caller can see if size is correct
+	writeBuffer.Flush()
+	stat, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	return int(stat.Size()), nil
 }
